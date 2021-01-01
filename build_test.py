885049f17af1ecdb30cfa99af1402ef056a0efbf
Yunfan Hu <1033718596@qq.com>
2021-01-01 12:37:12 +0800

    model and test

---
+++ build_test.py
@@ -0,0 +1,84 @@
+import os
+import json
+import pickle
+import argparse
+import math
+import pandas as pd
+import numpy as np
+import multiprocessing as mp
+import random
+from tqdm import tqdm
+
+random.seed(7)
+
+def build_examples(rank, args, df, news_info, fout):
+    data_list = []
+    for hist, imp in tqdm(df[["hist", "imp"]].values, total=df.shape[0]):
+        if str(hist) == 'nan':
+            his_list = []
+        else:
+            his_list = str(hist).strip().split()
+
+        his_idx_list = [news_info[h] for h in his_list]
+        hislen = len(his_idx_list)
+        if hislen < args.max_hist_length:
+            for _ in range(args.max_hist_length - hislen):
+                his_idx_list.append(0)
+        else:
+            his_idx_list = his_idx_list[-args.max_hist_length:]
+
+        imp_list = str(imp).split(' ')
+        clabel = 0
+        for impre in imp_list:
+            curn = news_info[impre]
+            label = clabel
+            clabel = clabel ^ 1
+
+            new_row = []
+            new_row.append(label)
+            new_row.append(curn)
+            new_row += his_idx_list
+            data_list.append(new_row)
+    
+    datanp = np.array(data_list, dtype=int)
+    np.save(fout, datanp)
+
+def main(args):
+    f_train_beh = os.path.join("data", args.fsamples)
+    df = pd.read_csv(f_train_beh, sep="\t", encoding="utf-8", names=["id", "uid", "time", "hist", "imp"])
+    news_info = json.load(open('data/news.json', 'r', encoding='utf-8'))
+
+    subdf_len = math.ceil(len(df) / args.processes)
+    cut_indices = [x * subdf_len for x in range(1, args.processes)]
+    dfs = np.split(df, cut_indices)
+
+    processes = []
+    for i in range(args.processes):
+        output_path = os.path.join("data", args.fout,  "{}-{}.npy".format(args.ftype, i))
+        p = mp.Process(target=build_examples, args=(
+            i, args, dfs[i], news_info, output_path))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    # Path options.
+    parser.add_argument("--fsamples", default="test/behaviors.tsv", type=str,
+                        help="Path of the training samples file.")
+    parser.add_argument("--fout", default="raw", type=str,
+                        help="Path of the output dir.")
+    parser.add_argument("--ftype", default="test", type=str,
+                        help="train or dev")
+    parser.add_argument("--max_hist_length", default=100, type=int,
+                        help="Max length of the click history of the user.")
+    parser.add_argument("--processes", default=40, type=int,
+                        help="Processes number")
+
+    args = parser.parse_args()
+
+    main(args)
+
---
+++ libfm.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 4/4/2019
+@author: RuihongQiu
+"""
+
+
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class LibFM(nn.Module):
+    """
+    Args:
+        hidden_size: the number of units in a hidden layer.
+        n_node: the number of items in the whole item set for embedding layer.
+    """
+    def __init__(self, news_num, dim_num=100):
+        super(LibFM, self).__init__()
+        self.embeddingL = nn.Embedding(news_num, 1)
+        self.embeddingQ = nn.Embedding(news_num, dim_num)
+        self.bias = nn.parameter.Parameter(torch.randn((1)).type(torch.FloatTensor))
+        self.news_num = news_num
+
+    def forward(self, X):
+        # X batch_size, 1 + his_len
+        eL = self.embeddingL(X)
+        logitL = eL.sum(dim=1, keepdim=True)
+
+        eQ = self.embeddingQ(X)
+        logitFM1 = eQ.mul(eQ).sum(1, keepdim=True).sum(2, keepdim=True)
+
+        z = eQ.sum(1, keepdim=True)
+        z2 = z.mul(z)
+        logitFM2 = z2.sum(dim=2, keepdim=True)
+
+        logitFM = (logitFM1 - logitFM2) * 0.5
+
+        logit = (logitL + logitFM).squeeze()
+        logit = logit + self.bias.expand(1, logit.size()[0]).permute(0, 1)
+  
+        return torch.sigmoid(logit)
