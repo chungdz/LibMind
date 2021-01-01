0bfbdb86f8e594685c33f18540d844a57c0840c1
Yunfan Hu <1033718596@qq.com>
2021-01-01 14:20:43 +0800

    rename

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 python build_train.py --processes=10 --ftype=train
 python build_train.py --processes=10 --ftype=dev --fsamples=valid/behaviors.small.tsv
 python build_test.py --processes=40
+CUDA_VISIBLE_DEVICES=0,1,2,3 python training.py --dataset=retailrocket --gpus=4
